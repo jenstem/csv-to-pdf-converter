38b184e34b7e06943ce73b818490a786e7ff3202
Jen <jennifer.stemkowski@gmail.com>
2024-04-17 09:21:26 -0400

    Add functionality to open CSV file

--- converter.py
+++ converter.py
@@ -3,5 +3,12 @@ from fpdf.fonts import FontFace
 import csv
 
 
+with open("countries.txt", encoding="utf-8") as csv_file:
+    data = list(csv.reader(csv_file, delimiter=","))
+
+
 pdf = FPDF()
 pdf.set_font("Arial", size=14)
+
+
+pdf.add_page()
